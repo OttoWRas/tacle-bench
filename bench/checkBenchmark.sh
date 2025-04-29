bb778b223785633fb8d0cec857307b0305a28a52
Otto Westy Rasmussen <otto.westy.rasmussen@gmail.com>
2025-04-29 16:56:06 +0200

    Update checkBenchmark.sh to require exec_option and append bench_tag to BENCH_NAME

--- bench/checkBenchmark.sh
+++ bench/checkBenchmark.sh
@@ -8,11 +8,20 @@ OPTIONS=" -O2 "
 #EXEC= # Adapt if the executable is to be executed via another program
 #EXEC=valgrind\ -q
 EXEC=~/t-crest/simulator/build/src/pasim
-EXEC_OPTIONS=" -S block -M fifo --mbsize 8 --full --hitmiss-stats "
 
 PASS=0
 FAIL_COMP=0
 FAIL_EXEC=0
+
+# Check if exec_option is provided as a command-line argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <exec_option>"
+    exit 1
+fi
+exec_option="${@:2}"
+# Extract the bench tag from the first argument
+bench_tag="$1"
+
 
 for dir in */; do
 
@@ -26,6 +35,8 @@ for dir in */; do
         # Remove trailing slash from BENCH
         BENCH_NAME=$(basename "$BENCH")
         
+        # Append the exec_prefix to BENCH_NAME
+        BENCH_NAME="${BENCH_NAME}_${bench_tag}"
 
         printf "Checking ${BENCH_NAME} ..."
         if [ -f a.out ]; then
@@ -43,7 +54,7 @@ for dir in */; do
         if [ -f a.out ]; then
             rm -f "$BENCH_NAME.pasim"
             touch "$BENCH_NAME.pasim"
-            $EXEC $EXEC_OPTIONS ./a.out 2> "$BENCH_NAME.pasim"
+            $EXEC $exec_option ./a.out 2> "$BENCH_NAME.pasim"
             RETURNVALUE=$(echo $?)
             if [ $RETURNVALUE -eq 0 ]; then
                 printf "passed. \n"
